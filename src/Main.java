8c576ba7049c6706078945765ce7c194c2dcb285
Samuel Baker <samuelcastro.baker@gmail.com>
2023-03-09 22:38:57 -0300

    Código e metricas

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //comentarios
+        System.out.println("Engenharia Econômica para Software");
+
+        /*
+        Varios
+        comentários
+        aqui
+         */
+
+        if(true){
+            int a = 0; int b = 0;
+        }
+    }
+}
